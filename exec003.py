7ea8f51ec0d9417017827d9dc4760b7fe1b6bce0
Bru/\/a <121907221+brugiamboni@users.noreply.github.com>
2024-04-11 10:50:42 -0300

    exec003

---
+++ exec003.py
@@ -0,0 +1,10 @@
+print('==== DESAFIO 03 ====')
+
+print('CALCULADORA DE SOMAS')
+
+n1 = int(input('Escolha o primeiro número a ser somado:'))
+n2 = int(input('Escolha o primeiro número a ser somado:'))
+
+s = n1 + n2
+
+print('O valor resultante da soma entre {} e {} é: {}'.format(n1, n2, s))
