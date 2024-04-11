980477514c2ecaaa6fd81a7fa2591fc05ce60831
Bru/\/a <121907221+brugiamboni@users.noreply.github.com>
2024-04-11 11:42:57 -0300

    exec005

---
+++ exec005.py
@@ -0,0 +1,17 @@
+print('==== DESAFIO 05 ====')
+
+termo = input('Vamos analisar o tipo do caractere que será digitado por você! Escreva algo:')
+
+print('Todos os caracteres digitados são números ou letras?\n', termo.isalnum())
+
+print('Qual o tipo do termo "{}" que foi digitado pelo usuário?\n'.format(termo), type(termo))
+
+print('"{}" é do tipo numérico?\n'.format(termo), termo.isnumeric())
+
+print('"{}" tem somente caracteres numéricos?\n'.format(termo), termo.isdigit())
+
+print('"{}" é um número decimal?\n'.format(termo), termo.isdecimal())
+
+print('"{}" está escrito em minúsculo?\n'.format(termo), termo.islower())
+
+print('"{}" tem as letras iniciais estão em maiúsculo?\n'.format(termo), termo.istitle())
