608a62571c6e76f73b583a8fd10aabec046e0e93
Bru/\/a <121907221+brugiamboni@users.noreply.github.com>
2024-04-11 10:33:16 -0300

    exec002

---
+++ exec002.py
@@ -0,0 +1,13 @@
+print('====== DESAFIO 02 ======')
+
+print('CALCULADORA DE SOMAS')
+
+# Solicita os números como entrada.
+Pnumero = int(input('Primeiro número para cálculo: '))
+Snumero = int(input('Segundo número para cálculo: '))
+
+# Realiza a soma.
+soma = Pnumero + Snumero
+
+# Imprime o resultado.
+print('A soma dos números é', soma)
