d2e81e3d798bee145ffcd169c217bbee4485bcc2
Bru/\/a <121907221+brugiamboni@users.noreply.github.com>
2024-04-11 12:08:57 -0300

    Script para identificar o nome que foi digitado.

--- exec001.py
+++ exec001.py
@@ -1,5 +1,7 @@
+# Script para identificar o nome que foi digitado.
+
 print('===== DESAFIO 01 =====')
 
 nome = input('Qual é seu nome?')
 
-print('É um prazer te conhecer',nome, '!')
+print('É um prazer te conhecer', nome, '!')
